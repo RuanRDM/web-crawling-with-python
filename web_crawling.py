210493185b01f7a5ceda8c161084a163b66a7e21
RuanRDM <ruan.delatorre.rd@gmail.com>
2023-11-20 20:06:07 -0300

    add indexed pages

--- web_crawling.py
+++ web_crawling.py
@@ -1,3 +1,4 @@
+import os
 import logging
 from urllib.parse import urljoin
 import requests
@@ -7,7 +8,6 @@ logging.basicConfig(
     format='%(asctime)s %(levelname)s:%(message)s',
     level=logging.INFO)
 
-# Adicione estas linhas para configurar um manipulador de arquivo
 file_handler = logging.FileHandler('crawler.log')
 file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s:%(message)s'))
@@ -18,6 +18,7 @@ class Crawler:
     def __init__(self, urls=[]):
         self.visited_urls = []
         self.urls_to_visit = urls
+        self.indexed_pages = {}
 
     def download_url(self, url):
         return requests.get(url).text
@@ -34,10 +35,20 @@ class Crawler:
         if url not in self.visited_urls and url not in self.urls_to_visit:
             self.urls_to_visit.append(url)
 
+    def save_page_to_file(self, url, html):
+        # Gera um nome de arquivo baseado no URL
+        filename = f'{url.replace("://", "_").replace("/", "_")}.html'
+        filepath = os.path.join('indexed_pages', filename)
+
+        with open(filepath, 'w', encoding='utf-8') as file:
+            file.write(html)
+
     def crawl(self, url):
         html = self.download_url(url)
-        for url in self.get_linked_urls(url, html):
-            self.add_url_to_visit(url)
+        self.indexed_pages[url] = html
+        self.save_page_to_file(url, html)  # Salva o conteúdo HTML da página indexada em um arquivo
+        for linked_url in self.get_linked_urls(url, html):
+            self.add_url_to_visit(linked_url)
 
     def run(self):
         while self.urls_to_visit:
@@ -51,4 +62,11 @@ class Crawler:
                 self.visited_urls.append(url)
 
 if __name__ == '__main__':
-    Crawler(urls=['https://quotes.toscrape.com/']).run()
+    # Cria o diretório para armazenar as páginas indexadas
+    os.makedirs('indexed_pages', exist_ok=True)
+
+    crawler = Crawler(urls=['https://quotes.toscrape.com/'])
+    crawler.run()
+
+    # Agora você pode acessar o conteúdo HTML indexado das páginas usando crawler.indexed_pages
+    # Exemplo: print(crawler.indexed_pages['https://quotes.toscrape.com/'])
